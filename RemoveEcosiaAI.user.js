355338ccf31e618b4320f176ce0a6ee8abb0ca8b
Tremco <119231037+Tremco@users.noreply.github.com>
2026-05-20 19:20:47 +0200

    Create RemoveEcosiaAI.user.js

---
+++ RemoveEcosiaAI.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name          Remove AI from Ecosia
+// @namespace     https://github.com/Tremco/remove-ecosia-ai
+// @version       1.0
+// @description   Removes AI Overviews and other AI related UI elements from ecosia.org.
+// @author        Tremco
+// @match         https://*.ecosia.org/*
+// @grant         none
+// @homepageURL   https://github.com/Tremco/remove-ecosia-ai
+// @license       MIT
+// @run-at        document-start
+// ==/UserScript==
+ 
+//remove AI Overviews
+document.cookie = 'ECAIO=false; path=/; domain=.ecosia.org';
+ 
+//remove AI related UI elements
+function removeAIComponents()
+{
+    document.querySelectorAll(
+    [
+        'a[href*="/ai-chat"]', //removes the AI UI in search suggestions and navigation bar
+        '[data-test-id="ai-overviews-field"]' //removes "Overviews" in Ecosia settings
+ 
+    ].join(',')).forEach(el =>
+    {
+        (el.closest('li') || el.closest('.search-navigation__item') || el.closest('.field') || el).remove();
+    });
+}
+ 
+new MutationObserver(removeAIComponents).observe(document.documentElement,
+{
+    childList: true,
+    subtree: true
+});
+ 
+window.addEventListener('DOMContentLoaded', removeAIComponents);
