10fb1c229a60d5b0dd5b6bf9412da437c35eab9e
Tremco <119231037+Tremco@users.noreply.github.com>
2026-05-21 14:45:27 +0200

    fix a bug where all URLs containing "/ai-chat" were filtered out of search results

--- RemoveEcosiaAI.user.js
+++ RemoveEcosiaAI.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Remove AI from Ecosia
 // @namespace     https://github.com/Tremco/remove-ecosia-ai
-// @version       1.0
+// @version       1.1
 // @description   Removes AI Overviews and other AI related UI elements from ecosia.org.
 // @author        Tremco
 // @match         https://*.ecosia.org/*
@@ -19,9 +19,10 @@ function removeAIComponents()
 {
     document.querySelectorAll(
     [
-        'a[href*="/ai-chat"]', //removes the AI UI in search suggestions and navigation bar
+        '[data-test-id="search-navigation-item-chat"]', //removes the AI UI in the navigation bar
+        '[data-test-id="search-form-suggestions"] [href*="/ai-chat"]', //removes AI in search suggestions
         '[data-test-id="ai-overviews-field"]' //removes "Overviews" in Ecosia settings
- 
+     
     ].join(',')).forEach(el =>
     {
         (el.closest('li') || el.closest('.search-navigation__item') || el.closest('.field') || el).remove();
